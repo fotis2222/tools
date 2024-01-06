ebaa4f719d2e1c2efb8a6d5808d940cb7c5c738e
fotis2222 <154263053+fotis2222@users.noreply.github.com>
2024-01-06 14:15:02 +0000

    added dice roller

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 print("""
 select a mode:
 (1) hypotenuse calc
+(2) dice roller
 more coming soon...""")
 mode = input()
 if mode == "1":
@@ -12,8 +13,13 @@ if mode == "1":
     print("height: ")
     h = float(input())
     print(f"hypotenuse: {math.hypot(b, h)}")
+elif mode == "2":
+    print("sides of dice: ")
+    sides = int(input())
+    print(f"you rolled a: {rd.randint(1, sides)}")
 else:
     print("press the number next to the mode you want")
 
+#exit sequence
 print("press enter to exit")
 input()
